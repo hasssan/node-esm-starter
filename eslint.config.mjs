ccd4063f94a1db83dbb58751b95070ce3987e25a
A. Hassan <git@hassan.web.id>
2025-04-19 19:41:08 +0700

    feat: update eslint config flat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,16 @@
 import { defineConfig } from "eslint/config";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import prettierConfig from "eslint-config-prettier/flat";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended"),
-
+export default defineConfig([
+  js.configs.recommended,
+  prettierConfig,
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
     },
-
-    rules: {},
-}]);
+  },
+]);
